58e549a27d03f5ee3672d0892c1b563256fe3b52
lowwin_bzn <mistselene.zl@gmail.com>
2019-05-11 13:27:33 +0900

    表示周りの整理

--- kotlin/domemo.kt
+++ kotlin/domemo.kt
@@ -29,29 +29,36 @@ fun fieldOpenPieceCount(playerCount:Int) = when(playerCount){
 }
 
 fun main(args:Array<String>){
+    val playerCount = 5
+
     val pieces = (1..7).flatMap{i -> (1..i).map{_ -> i}}.toMutableList()
     pieces.shuffle()
-    val playerCount = 5
     val handPiece = handPieceCount(playerCount)
     val players = (0..playerCount-1).map{id -> Player("Player" + (id + 1), pieces.drop(id * handPiece).take(handPiece).toMutableList())}
-    val fieldOpenPieces = pieces.drop(handPiece * playerCount).take(fieldOpenPieceCount(playerCount))
-    for(p in players){
-        p.show()
-    }
-    println("field opened pieces: " + fieldOpenPieces)
+    val fieldOpenPieces = pieces.drop(handPiece * playerCount).take(fieldOpenPieceCount(playerCount)).toMutableList()
 
     var currentPlayer = 0
     val picker = SimpleRandomPicker()
     while(players.all{p -> !p.isAllOpen()}){
         val p = players[currentPlayer]
         println("${p.name}'s Turn...")
+        val otherPlayers = players.take(currentPlayer) + players.drop(currentPlayer + 1).take(players.size - currentPlayer + 1)
+        println("field opened pieces: " + fieldOpenPieces)
+
+        println("other player pieces")
+        for(other in otherPlayers){
+            other.show()
+        }
+        println()
+
         val choiceNum = picker.pick()
         println("choice number: ${choiceNum}")
         if(p.hasPiece(choiceNum)){
             println("has Piece! ${choiceNum}")
             p.remove(choiceNum)
+            fieldOpenPieces.add(choiceNum)
         }
-        currentPlayer = if(currentPlayer == players.size - 1) 0 else currentPlayer + 1
+        currentPlayer = if(currentPlayer + 1 == players.size) 0 else currentPlayer + 1
         println()
     }
 
